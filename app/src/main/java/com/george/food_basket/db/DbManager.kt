ceed96835582e5d15c10a2c905fffb6c29cefb53
George Magpie <prich845@gmail.com>
2021-01-19 12:41:10 +0300

    Initial commit

---
+++ app/src/main/java/com/george/food_basket/db/DbManager.kt
@@ -0,0 +1,41 @@
+package com.george.fridge.db
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+
+class DbManager(context : Context) {
+    val ProductsDBHelper = ProductsDBHelper(context)
+
+    var db: SQLiteDatabase? = null
+
+    fun openDB(){
+        db = ProductsDBHelper.writableDatabase
+    }
+
+    fun insertToDB(product: String){
+        val values = ContentValues().apply {
+            put(ProductsContract.COLUMN_NAME_TITLE, product)
+        }
+        db?.insert(ProductsContract.TABLE_NAME, null, values)
+    }
+
+    fun readDbData(): ArrayList<String>{
+        val dataList = ArrayList<String>()
+
+        val cursor = db?.query(ProductsContract.TABLE_NAME,  null, null, null, null, null, null,)
+
+        with(cursor){
+            while (this?.moveToNext()!!){
+                val dataText = cursor?.getString(cursor.getColumnIndex(ProductsContract.COLUMN_NAME_TITLE))
+                dataList.add(dataText.toString())
+            }
+        }
+        cursor?.close()
+        return dataList
+    }
+
+    fun closeDb(){
+        ProductsDBHelper.close()
+    }
+}
